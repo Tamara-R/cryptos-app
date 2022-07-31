087292a4287535482066a0582457b4d0744bd98f
Tamara-R <tamara.ranisavljevic@gmail.com>
2022-07-31 21:58:26 +0200

    Updated

---
+++ src/constants/liveSymbols.js
@@ -0,0 +1,14 @@
+export const ADD_BTCUSD_WS = "ADD_BTCUSD_WS"
+export const GET_BTCUSD_WS = "GET_BTCUSD_WS"
+
+export const ADD_ETHBTC_WS = "ADD_ETHBCT_WS"
+export const GET_ETHBTC = "GET_ETHBCT"
+
+export const ADD_ETHUSD_WS = "ADD_ETHUSD_WS"
+export const GET_ETHUSD_WS = "GET_ETHUSD_WS"
+
+export const ADD_LTCBTC_WS = "ADD_LTCBTC_WS"
+export const GET_LTCBTC_WS = "GET_LTCBTC_WS"
+
+export const ADD_LTCUSD_WS = "ADD_LTCUSD_WS"
+export const GET_LTCUSD_WS = "GET_LTCUSD_WS"
