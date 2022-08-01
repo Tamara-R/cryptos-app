5ca20d482aa3d10b2f3271d35fdf26bb42c74592
Tamara-R <tamara.ranisavljevic@gmail.com>
2022-08-01 23:45:01 +0200

    Updated

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -11,7 +11,7 @@ export const authReducer = ( state = {}, action) => {
         case LOGOUT: 
             return {
                 ...state,
-                user: {},
+                user: null,
                 isAuth: false
             }
         default: 
